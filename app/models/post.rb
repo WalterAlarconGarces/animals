24eb156e2be35e8b8a9ff9977f113b9079952e3c
Walter Alarcon Garces <walarcon@gmail.com>
2023-08-20 19:29:22 -0400

    modificacion estilos _post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
     belongs_to :user
     has_many :comments, as: :commentable, dependent: :destroy
-    has_many :likes
+    has_many :likes, dependent: :destroy
     has_one_attached :image
     include PgSearch::Model
  pg_search_scope :search_full_text,
