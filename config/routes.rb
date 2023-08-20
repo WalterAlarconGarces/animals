26bac18826748df10010b276e039d2101b3841ba
Walter Alarcon Garces <walarcon@gmail.com>
2023-08-20 01:12:33 -0400

    creacion de imagen de perfil

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions', registrations: 'users/registrations'
+  }
+
   resources :posts do
     resources :comments, only: :create
   end
